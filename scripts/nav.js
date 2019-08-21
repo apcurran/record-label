6f2e8e474e6c7d3308e89754d7511d0f7583004d
apc <alexcurran.va@gmail.com>
2019-08-21 12:32:36 -0500

    Finished nav functionality

---
+++ scripts/nav.js
@@ -0,0 +1,17 @@
+const navModule = (() => {
+    const burgerIcon = document.querySelector(".burger-nav-icon");
+    const nav = document.querySelector(".nav");
+    const closeBtn = document.querySelector(".nav-section-close");
+
+    function openNav(event) {
+        nav.classList.add("nav-active");
+    }
+
+    function closeNav(event) {
+        nav.classList.remove("nav-active");
+    }
+
+    burgerIcon.addEventListener("click", openNav);
+    closeBtn.addEventListener("click", closeNav);
+
+})();
